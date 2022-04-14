0c21757dd164a916b4659ec970c743a411e6dae7
babuamrit <92739498+babuamrit@users.noreply.github.com>
2022-04-15 01:08:15 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { Box, Button, Modal, Typography } from "@mui/material";
 import React from "react";
 import Footer from "../pagecomponents/Footer/Footer";
 import Header from "../pagecomponents/Header/Header";
@@ -9,6 +10,7 @@ const home = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Header />
+      <BasicModal />
       <Menu />
       <Services />
       <Vibes />
@@ -18,3 +20,43 @@ const home = () => {
 };
 
 export default home;
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Frontend task done by Amrit Babu Tiwari (babuamrit25@gmail.com)
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            kriti Shrestha, HR Department, Pagoda Labs Pvt. Ltd, Kumaripati,
+            Lalitpur, Contact number:9841142514
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
